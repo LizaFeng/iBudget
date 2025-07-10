edeacdc688f81262386c8f405e225cc1cf8236c0
Liza Feng <liza.feng05@myhunter.cuny.edu>
2025-07-10 14:14:24 -0400

    setting up the basic routing of navbar

--- iBudget/src/app/app.routes.ts
+++ iBudget/src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateBudget } from './create-budget/create-budget';
+import { HomeComponent } from './home-component/home-component';
+import { UserInfo } from './user-info/user-info';
+import { PageNotFound } from './page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'create-budget', component: CreateBudget },
+  { path: 'user-info', component: UserInfo },
+  { path: '**', component: PageNotFound },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
