5289edf0626ce7724a1a9e79d237e6c38392c674
Liza Feng <liza.feng05@myhunter.cuny.edu>
2025-07-21 00:28:33 -0400

    fixing the routing for the budget pdf card and adding form validation reactive

--- iBudget/src/app/budget-pdf-card/budget-pdf-card.ts
+++ iBudget/src/app/budget-pdf-card/budget-pdf-card.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-budget-pdf-card',
   imports: [],
   templateUrl: './budget-pdf-card.html',
-  styleUrl: './budget-pdf-card.css'
+  styleUrl: './budget-pdf-card.css',
 })
-export class BudgetPdfCard {
+export class BudgetPdfCard {}
 
-}
+//TODO:july 18 figure out how to import this component to the User-info component
--- iBudget/src/app/user-info/user-info.ts
+++ iBudget/src/app/user-info/user-info.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { BudgetPdfCard } from '../budget-pdf-card/budget-pdf-card';
 
 @Component({
   selector: 'app-user-info',
-  imports: [],
+  imports: [BudgetPdfCard],
   templateUrl: './user-info.html',
-  styleUrl: './user-info.css'
+  styleUrl: './user-info.css',
 })
-export class UserInfo {
-
-}
+export class UserInfo {}
