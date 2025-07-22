545f711185713eb4f483cfb1356f285604bae479
Liza Feng <liza.feng05@myhunter.cuny.edu>
2025-07-22 13:29:52 -0400

    making the default income component

--- iBudget/src/app/default-income-component/default-income-component.ts
+++ iBudget/src/app/default-income-component/default-income-component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-default-income-component',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './default-income-component.html',
-  styleUrl: './default-income-component.css'
+  styleUrl: './default-income-component.css',
 })
-export class DefaultIncomeComponent {
-
-}
+export class DefaultIncomeComponent {}
