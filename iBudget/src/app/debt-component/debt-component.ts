aa99096771055034bf1d0c6a29c5da8b64c50795
Liza Feng <liza.feng05@myhunter.cuny.edu>
2025-07-28 14:09:17 -0400

    default component basic outline completed

--- iBudget/src/app/debt-component/debt-component.ts
+++ iBudget/src/app/debt-component/debt-component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, input } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { Input } from '@angular/core';
 @Component({
   selector: 'app-debt-component',
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './debt-component.html',
-  styleUrl: './debt-component.css'
+  styleUrl: './debt-component.css',
 })
 export class DebtComponent {
-
+  @Input() defaultCard: boolean = false;
 }
