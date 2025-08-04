cf752cd65cb173b1e2d52ceefa6e5ba51b8bea2b
Liza Feng <liza.feng05@myhunter.cuny.edu>
2025-08-04 15:21:08 -0400

    refactored savings component to use mat card

--- iBudget/src/app/savings-component/savings-component.ts
+++ iBudget/src/app/savings-component/savings-component.ts
@@ -1,9 +1,28 @@
 import { Component, Input } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MatFormField,
+  MatFormFieldModule,
+  MatLabel,
+} from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-savings-component',
-  imports: [MatIcon],
+  imports: [
+    MatIcon,
+    MatLabel,
+    MatFormField,
+    MatFormFieldModule,
+    MatInput,
+    MatSelect,
+    MatSelectModule,
+    MatInputModule,
+    MatSelect,
+    MatSelectModule,
+  ],
   templateUrl: './savings-component.html',
   styleUrl: './savings-component.css',
 })
