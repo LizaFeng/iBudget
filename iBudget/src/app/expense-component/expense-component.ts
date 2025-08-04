4e3a3ce8e22db4fb7997d9f9ecf64bf098a6ec62
Liza Feng <liza.feng05@myhunter.cuny.edu>
2025-08-04 14:19:16 -0400

    expense component refactored to use mat card

--- iBudget/src/app/expense-component/expense-component.ts
+++ iBudget/src/app/expense-component/expense-component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
 
 @Component({
   standalone: true,
   selector: 'app-expense-component',
-  imports: [MatIcon],
+  imports: [
+    MatIcon,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatSelect,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+  ],
+  providers: [MatDatepickerModule],
   templateUrl: './expense-component.html',
   styleUrl: './expense-component.css',
 })
